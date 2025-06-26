a4282ee9826cdfe9fb7b7f730ccd6a3fb209e133
PeterKoffer <peter@2ne.dk>
2025-06-26 11:16:42 +0200

    new testpage

---
+++ src/pages/AdaptivePracticeTestPage.tsx
@@ -0,0 +1,17 @@
+// src/pages/AdaptivePracticeTestPage.tsx
+import React from 'react';
+// Assuming AdaptivePracticeModule is now at src/components/adaptive-learning/AdaptivePracticeModule.tsx
+import AdaptivePracticeModule from '../components/adaptive-learning/AdaptivePracticeModule';
+
+const AdaptivePracticeTestPage: React.FC = () => {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Adaptive Practice Module Test Page</h1>
+      <p>This page is for testing the end-to-end flow of the adaptive practice module.</p>
+      <hr style={{ margin: '20px 0' }} />
+      <AdaptivePracticeModule />
+    </div>
+  );
+};
+
+export default AdaptivePracticeTestPage;
