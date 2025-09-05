ec090a4ec46f14c26a1d0a6e320c8fd27f42507f
Peter Kofoed <peter@2ne.dk>
2025-09-05 14:28:11 +0200

    Refactor Index component to export Landing directly

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,8 +1,2 @@
-import React from "react";
-import Landing from "@/features/shell/pages/Landing";
-
-export default function Index() {
-  return <Landing />;
-}
-// src/pages/Index.tsx
-export { default } from "@/features/shell/pages/Landing";
+// src/pages/Auth.tsx
+export { default } from "@/features/auth/pages/Auth";
