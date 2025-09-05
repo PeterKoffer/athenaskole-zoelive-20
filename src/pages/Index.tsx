53ca9602e64855546ef845082f0e948aa0f2fdb8
Peter Kofoed <peter@2ne.dk>
2025-09-05 13:50:13 +0200

    Export Landing component from Index page

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -4,3 +4,5 @@ import Landing from "@/features/shell/pages/Landing";
 export default function Index() {
   return <Landing />;
 }
+// src/pages/Index.tsx
+export { default } from "@/features/shell/pages/Landing";
