6f7b07b4bc40623603bfc67f9ad1996306448ca8
PeterKoffer <peter@2ne.dk>
2025-09-14 14:44:57 +0200

    feat(NELIE): single-instance baseline; 2x size; no ring

---
+++ src/components/NELIE/NELIE.tsx
@@ -0,0 +1,8 @@
+// src/components/NELIE/NELIE.tsx
+import React from "react";
+import FloatingNELIE from "./floating/RefactoredFloatingAITutor";
+
+export default function NELIE() {
+  // Ét centralt sted at konfigurere figuren (tema, størrelse, events) senere
+  return <FloatingNELIE />;
+}
