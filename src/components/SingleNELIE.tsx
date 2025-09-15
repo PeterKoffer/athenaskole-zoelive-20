d05d65ca95eec8b1f9451a01a3d556aed3c7e883
Peter Kofoed <peter@2ne.dk>
2025-09-15 08:43:54 +0200

    Use React portal in SingleNELIE component
    Refactor SingleNELIE component to use React portal for rendering.

--- src/components/SingleNELIE.tsx
+++ src/components/SingleNELIE.tsx
@@ -1,2 +1,11 @@
-import NELIE from "@/components/NELIE/NELIE";
-export default function SingleNELIE() { /* … */ }
+import React from "react";
+import { createPortal } from "react-dom";
+
+export default function SingleNELIE() {
+  const node = (
+    <div className="fixed top-6 left-6 z-[99999] pointer-events-none select-none" aria-hidden>
+      <img src="/nelie.png" alt="NELIE" width={96} height={96} className="nelie-avatar block" draggable={false} />
+    </div>
+  );
+  return createPortal(node, document.body);
+}
